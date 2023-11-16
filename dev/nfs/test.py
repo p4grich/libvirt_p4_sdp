ee1550e70f457b9c9c8141367dfa8c1d15d8a741
p4grich <46497559+p4grich@users.noreply.github.com>
2023-11-15 18:11:18 -0800

    cleanup fixture

--- dev/nfs/test.py
+++ dev/nfs/test.py
@@ -4,22 +4,9 @@ import testinfra
 import json
 
 @pytest.fixture
-def get_ansible_vars(host):
-  global ansible_user, network_cidr, domain, hostname, fqdn
+def ansible_vars(host):
   a_vars = host.ansible.get_variables()
-  for k, v in a_vars.items():
-    if k == "network_cidr":
-      network_cidr = v
-    if k == "hostname":
-      hostname = v
-    if k == "ansible_user":
-      ansible_user = v
-    if k == "domain":
-      domain = v
-    if k == "fqdn":
-      fqdn = v
-    a_json=json.dumps(a_vars)
-  return ansible_user, network_cidr, domain, hostname, fqdn
+  return a_vars
 
 def test_os_release(host):
     assert host.file("/etc/os-release").contains("Rocky")
@@ -49,12 +36,14 @@ def test_dir_hxdepots(host):
     assert host.file("/nfs/hxdepots").uid == 65534
     assert host.file("/nfs/hxdepots").gid == 65534
 
-def test_file_export_commit(host, get_ansible_vars):
-    assert host.file("/etc/exports").content_string.splitlines()[0] == '/nfs/hxdepots ' + network_cidr +'(rw,sync,no_subtree_check)'
+def test_file_export_commit(host, ansible_vars):
+    print("Network: ", ansible_vars['network_cidr'])
+    assert host.file("/etc/exports").content_string.splitlines()[0] == '/nfs/hxdepots ' + ansible_vars['network_cidr'] + '(rw,sync,no_subtree_check)'
 
-def test_idmap_domain(host, get_ansible_vars):
+def test_idmap_domain(host, ansible_vars):
+    print("Domain: ",ansible_vars['domain'])
     assert host.file("/etc/idmapd.conf").is_file is True
-    assert host.file("/etc/idmapd.conf").content_string.splitlines()[4] == 'Domain = ' + domain
+    assert host.file("/etc/idmapd.conf").content_string.splitlines()[4] == 'Domain = ' + ansible_vars['domain']
 
 def test_service_nfs_server(host):
     assert host.service("nfs-server").is_enabled is True
@@ -63,4 +52,3 @@ def test_service_nfs_server(host):
 def test_service_rpcbind_server(host):
     assert host.service("rpcbind").is_enabled is True
     assert host.service("rpcbind").is_running is True
-
