cacd532239d77c34cb0ae1ae75bb975a85503628
p4grich <46497559+p4grich@users.noreply.github.com>
2023-11-28 10:42:11 -0800

    add docstring for pylint

--- dev/commit/test.py
+++ dev/commit/test.py
@@ -1,12 +1,15 @@
 import testinfra
 
 def test_os_release(host):
+    """Test OS release"""
     assert host.file("/etc/os-release").contains("Rocky")
 
 def test_sshd_inactive(host):
+    """Test SSH service"""
     assert host.service("sshd").is_running is True
 
 def test_perforce_user(host):
+    """Test Perforce user"""
     assert host.user("perforce").exists is True
     assert host.user("perforce").uid == 9004
     assert host.user("perforce").gid == 9004
@@ -16,24 +19,28 @@ def test_perforce_user(host):
     assert host.user("perforce").password == "!!"
 
 def test_nfs_hxdepots_mount(host):
+    """Test SDP base dir/mount"""
     assert host.file("/hxdepots").exists is True
     assert host.file("/hxdepots").uid == 9004
     assert host.file("/hxdepots").gid == 9004
     assert host.mount_point("/hxdepots").exists is True
 
 def test_dir_hxlogs(host):
+    """Test SDP logs dir"""
     assert host.file("/hxlogs").exists is True
     assert host.file("/hxlogs").uid == 9004
     assert host.file("/hxlogs").gid == 9004
     assert host.mount_point("/hxlogs").exists is False
 
 def test_dir_hxmetadata(host):
+    """Test SDP metadata dir"""
     assert host.file("/hxmetadata").exists is True
     assert host.file("/hxmetadata").uid == 9004
     assert host.file("/hxmetadata").gid == 9004
     assert host.mount_point("/hxmetadata").exists is False
 
 def test_package_install(host):
+    """Test SDP software deps"""
     assert host.package("unzip").is_installed
     assert host.package("bind-utils").is_installed
     assert host.package("unzip").is_installed
